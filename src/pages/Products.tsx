e8d8bd9ea772f724a6ba9cba0b1b54d0b49a7027
Rod Ritter <rod@ritter.co.za>
2022-11-02 02:00:48 +0200

    feat: created products page UI

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,18 +1,33 @@
 import BaseLayout from "../layouts/BaseLayout";
 import useProducts from "../hooks/useProducts";
-import ProductGrid from "../containers/ProductGrid";
+import useCart from "../hooks/useCart";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Products = () => {
     const { data } = useProducts();
+    const { addToCart } = useCart();
 
     return (
         <BaseLayout>
-            <ProductGrid>
+            <div className="product-grid">
                 {data &&
                     data.map((product: any) => (
-                        <div key={product.id}>{product.title}</div>
+                        <section className="product-card" key={product.id}>
+                            <div className="product-img-wrapper">
+                                <img src={product.image} alt={product.title} />
+                            </div>
+                            <h3 title={product.title}>{product.title}</h3>
+                            <h4>${product.price}</h4>
+                            <button
+                                className="add-to-cart-btn"
+                                onClick={() => addToCart(product)}
+                            >
+                                <FontAwesomeIcon icon={faCartShopping} /> Add
+                            </button>
+                        </section>
                     ))}
-            </ProductGrid>
+            </div>
         </BaseLayout>
     );
 };
